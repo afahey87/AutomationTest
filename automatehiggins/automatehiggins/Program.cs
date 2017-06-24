62cd0c0237985e715d21870635bac2e2894d207d
Adam Fahey <afahey87@gmail.com>
2017-06-23 20:26:15 -0400

    First Checkin
    First Checkin

---
+++ automatehiggins/automatehiggins/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace automatehiggins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.higgins3.com/");
+            IWebElement element = driver.FindElement(By.Id("s"));
+            element.Click();
+            element.SendKeys("Printer");
+            System.Threading.Thread.Sleep(2000);
+
+
+            IWebElement element2 = driver.FindElement(By.Id("searchsubmit"));
+            element2.Click();
+            System.Threading.Thread.Sleep(2000);
+
+
+
+        }
+    }
+}
